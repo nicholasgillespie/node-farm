75f1a9a34f9558cfbc51732de9a61ec6f5fd721c
Nicholas Gillespie <contact@nicholasgillespie.com>
2022-08-17 14:08:19 +0200

    17. exported replaceTemplate module

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
-/* MODULES ////////////////////////////// */
+/* MODULES - CORE ////////////////////////////// */
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
 
-/* FUNCTION ////////////////////////////// */
-const replaceTemplate = (temp, product) => {
-  let output = temp.replaceAll('{%PRODUCTNAME%}', product.productName);
-  output = output.replaceAll('{%IMAGE%}', product.image);
-  output = output.replaceAll('{%PRICE%}', product.price);
-  output = output.replaceAll('{%FROM%}', product.from);
-  output = output.replaceAll('{%NUTRIENTS%}', product.nutrients);
-  output = output.replaceAll('{%QUANTITY%}', product.quantity);
-  output = output.replaceAll('{%DESCRIPTION%}', product.description);
-  output = output.replaceAll('{%ID%}', product.id);
-
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-  return output;
-};
+/* MODULES - OWN ////////////////////////////// */
+const replaceTemplate = require('./modules/replaceTemplate');
 
 // FILES - TEMPLATES ////////////////////////////// */
 const tempOverview = fs.readFileSync(
